131b591f574003a2e62313d8cf221283f09d3b57
Leonardo Rodrigues Silva <re4gm@hotmail.com>
2023-01-31 14:02:17 -0300

    Add files via upload

---
+++ py_video.py
@@ -0,0 +1,15 @@
+import yt_dlp
+
+#get the list in the main folder
+lista = open('lista.txt', 'r')
+Lines = lista.readlines()
+
+#options about download quality
+ydl_opts = {
+    'format': 'best',
+    'nooverwrites': True,
+}
+# for every line in the list.txt
+for line in Lines:
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(line)
